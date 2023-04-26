b6be0de77e76a3fd9ed66787ea16895e4bc519b0
leverh <leverh@gmail.com>
2023-04-26 15:35:03 +0200

    Add maps.js file. Add maps and markers to interest page

---
+++ assets/JS/maps.js
@@ -0,0 +1,12 @@
+var map = L.map('map').setView([53.505, -0.09], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([53.5, -0.09]).addTo(map);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
